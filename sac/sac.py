66e698a53c439e602da8064016db05ed8b00e765
thachdovan <thachdovan@gmail.com>
2020-03-28 17:41:14 +0900

    update alg

--- sac/sac.py
+++ sac/sac.py
@@ -336,7 +336,7 @@ if __name__ == '__main__':
     from utils.run_utils import setup_logger_kwargs
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
 
-    sac(lambda : Ballbot2D_PID(), actor_critic=core.mlp_actor_critic,
+    sac(lambda : Ballbot2D_SK(), actor_critic=core.mlp_actor_critic,
         ac_kwargs=dict(hidden_sizes=[args.hid]*args.l),
         gamma=args.gamma, seed=args.seed, epochs=args.epochs,
         logger_kwargs=logger_kwargs)
